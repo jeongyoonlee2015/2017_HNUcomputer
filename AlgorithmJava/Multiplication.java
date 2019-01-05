e370c458e704fc87a46ed6c938f11eefe5cacce9
JeongyoonLee <43804152+jeongyoonlee2015@users.noreply.github.com>
2019-01-05 22:47:07 +0900

    Update Multiplication.java

--- AlgorithmJava/Multiplication.java
+++ AlgorithmJava/Multiplication.java
@@ -11,8 +11,9 @@ public class Multiplication {
      }
      }
      }
-     }*/
-    public static void main(String[] args){
+     }
+    */
+    /**public static void main(String[] args){
         while(true){
             System.out.println("\n구구단 연산을 할 단을 입력하세요.(종료: 0)");
             Scanner scanner = new Scanner(System.in);
@@ -29,4 +30,17 @@ public class Multiplication {
             }
         }
     }
+    */
+    public static void main(String[] args){
+        int[] result = new int[11];
+        for(int j = 2; j < result.length - 1; j++){
+            System.out.println("\n <"+ j +"단>");
+            for(int i = 1; i < result.length - 1; i++){
+                result[i] = j * i;
+                System.out.println(j + " * " + i + " = " + result[i]);
+
+            }
+        }
+    }
+
 }
