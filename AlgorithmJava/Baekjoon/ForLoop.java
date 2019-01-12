d472c0b172868b6856a2f3bd4ad298dcbf76cf7f
JeongyoonLee <43804152+jeongyoonlee2015@users.noreply.github.com>
2019-01-12 22:00:52 +0900

    Update ForLoop.java

--- AlgorithmJava/Baekjoon/ForLoop.java
+++ AlgorithmJava/Baekjoon/ForLoop.java
@@ -82,3 +82,21 @@ public class HelloWorld {
  //       scanner.close();
     }
 }
+/*8393*/
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		        Scanner scanner = new Scanner(System.in);
+		        int N = scanner.nextInt();
+		        int sum = 0;
+		        for(int i = 1; i <= N; i++) {
+
+		        	sum += i;		        
+		        }
+                System.out.println(sum);
+
+		 //       scanner.close();
+		    }
+	}
